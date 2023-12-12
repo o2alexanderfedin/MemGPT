c380cb1ab3fa88f2e5b8272caef09c1dce9d7a48
o2alex <af@O2.services>
2023-12-11 16:41:10 -0800

    small fix

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-from memgpt.main import app
+# import typer
+#
+# typer.secho(
+#     "Command `python main.py` no longer supported. Please run `memgpt run`. See https://memgpt.readthedocs.io/en/latest/quickstart/.",
+#     fg=typer.colors.YELLOW,
+# )
 
-app()
+
+def main_app():
+    from memgpt.main import app
+    app()
+
+
+if __name__ == "__main__":
+    main_app()
